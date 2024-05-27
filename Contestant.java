27ed33eb8bb00d2d5d69ba0ae8b6e55c5eae7839
Jeremy Penaredondo <jay.pena1224@proton.me>
2024-05-27 18:33:23 -0400

    creates constructors, getters, and setters to contestant

--- Contestant.java
+++ Contestant.java
@@ -1,3 +1,31 @@
 public class Contestant {
-    
+    private String name;
+    private String status = "Alive";
+    private static final String DEFAULT_NAME = "Contestant";
+
+    public Contestant() {
+        this(DEFAULT_NAME);
+    }
+
+    public Contestant(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getstatus() {
+        return this.status;
+    }
+
+    public void passAway() {
+        this.status = "Deceased";
+    }
+
+    // for testing purposes only
+    public static void main(String args[]) {
+        Contestant A = new Contestant("Mori");
+        System.out.println(A.getName());
+    }
 }
